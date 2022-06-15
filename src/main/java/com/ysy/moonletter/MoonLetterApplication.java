44c103f7c2c516164004349d38665f5c8ce0f61b
yenow <phantom_ysy@naver.com>
2022-06-15 22:27:55 +0900

    project start

---
+++ src/main/java/com/ysy/moonletter/MoonLetterApplication.java
@@ -0,0 +1,13 @@
+package com.ysy.moonletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonLetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonLetterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ysy/moonletter/MoonLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.moonletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonLetterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
